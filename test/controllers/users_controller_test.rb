77e45bbf0c192b75ce04dcbfc2e361305fe6adcc
David Santoso <david.e.santoso@gmail.com>
2015-01-17 23:10:31 -0500

    Ensure specific user actions return 404

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  test "index should return 404" do
+    get :index
+    assert_response :not_found
+  end
+
+  test "new should return 404" do
+    get :new
+    assert_response :not_found
+  end
+
+  test "show should return 404" do
+    get :show, {id: 1}
+    assert_response :not_found
+  end
+
+  test "edit should return 404" do
+    get :edit, {id: 1}
+    assert_response :not_found
+  end
 end
