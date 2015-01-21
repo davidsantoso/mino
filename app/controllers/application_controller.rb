674d4aa7a41a8aa2f8a0c3c06a9a85e4989e12ba
David Santoso <david.e.santoso@gmail.com>
2015-01-21 00:06:38 -0500

    Only respond to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :null_session, if: :json_request?
+  respond_to :json
 
   protected
 
