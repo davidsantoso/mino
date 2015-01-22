0f4ed60b2749f8e17d5d7f010acf12f07fb3b0d9
David Santoso <david.e.santoso@gmail.com>
2015-01-21 21:41:06 -0500

    Test user callback methods

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,21 @@ class UserTest < ActiveSupport::TestCase
   should validate_uniqueness_of(:email)
   should validate_uniqueness_of(:public_key)
   should validate_uniqueness_of(:encrypted_private_key)
+
+  test "should trigger setup_email_verification on save" do
+    @user = users(:one)
+
+    assert_respond_to @user, :setup_email_verification
+    @user.send(:setup_email_verification)
+
+    assert_equal false, @user.verified
+    assert_not_empty @user.email_verification_token
+  end
+
+  test "should respond to send_verification_token" do
+    @user = users(:one)
+    assert_respond_to @user, :setup_email_verification
+
+    # Should back fill test stubbing out email send
+  end
 end
