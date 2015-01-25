673c436693411e2c9148cc33258e2ffa3e6c687b
David Santoso <david.e.santoso@gmail.com>
2015-01-25 16:50:42 -0500

    Change email verification callbacks to only on creates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :public_key, :encrypted_private_key
   validates_uniqueness_of :email, :public_key, :encrypted_private_key
 
-  before_save :setup_email_verification
-  after_save :send_verification_token
+  before_create :setup_email_verification
+  after_create :send_verification_token
 
   def setup_email_verification
     self.verified = false
