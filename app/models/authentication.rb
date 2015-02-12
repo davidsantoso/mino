c6bc7d1face1e8db3e1d0676cd427caf132d8334
David Santoso <david.e.santoso@gmail.com>
2015-02-11 22:25:00 -0500

    Only create new authentications if a user account is verified

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,13 +1,17 @@
 class Authentication < ActiveRecord::Base
   validates_presence_of :message, :token
-  validates_uniqueness_of :message, :token
 
   belongs_to :user
 
   after_initialize :generate_message_and_token
+  before_save :validate_user_is_verified
 
   def generate_message_and_token
     self.message = SecureRandom.hex(32)
     self.token = SecureRandom.hex(32)
+  end
+
+  def validate_user_is_verified
+    errors.add(:user_id, "email address has not been verified") if !self.user.verified
   end
 end
--- test/controllers/authentications_controller_test.rb
+++ test/controllers/authentications_controller_test.rb
@@ -17,4 +17,26 @@ class AuthenticationsControllerTest < ActionController::TestCase
     assert_empty authentication.errors
     assert_response :success
   end
+
+  # POST /authentications currently returns the following hash
+  # if an account has not been verified yet:
+  #
+  # {
+  #   "errors": {
+  #     "user_id": [
+  #       "email address has not been verified"
+  #     ]
+  #   }
+  # }
+  #
+  # Error message makes sense, but the structure of the response
+  # could probably use some thought. Makes sense to use the "user_id"
+  # field since it's a field of the Authentication but...
+  #
+  # ¯\_(ツ)_/¯
+  #
+  test "should not create authentication if user is not verified" do
+    post :create, user: {email: "rick.grimes@mail.com"}
+    assert_response :conflict
+  end
 end
