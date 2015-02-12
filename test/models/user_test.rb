a8ac56cca6b28b30c5ef2e3d08063384685ca8a2
David Santoso <david.e.santoso@gmail.com>
2015-02-11 22:05:52 -0500

    Get outta here parens!

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  should validate_presence_of(:email)
-  should validate_presence_of(:public_key)
-  should validate_presence_of(:encrypted_private_key)
+  should validate_presence_of :email
+  should validate_presence_of :public_key
+  should validate_presence_of :encrypted_private_key
 
-  should validate_uniqueness_of(:email)
-  should validate_uniqueness_of(:public_key)
-  should validate_uniqueness_of(:encrypted_private_key)
+  should validate_uniqueness_of :email
+  should validate_uniqueness_of :public_key
+  should validate_uniqueness_of :encrypted_private_key
 
   test "should trigger setup_email_verification on save" do
     @user = users(:one)
