d7bf698251c99024798a10f74203251ea271ecba
David Santoso <david.e.santoso@gmail.com>
2015-02-17 21:15:27 -0500

    Back fill user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,8 +24,23 @@ class UserTest < ActiveSupport::TestCase
     assert_respond_to @user, :send_email_address_verification_token
   end
 
+  test "should send email_address_verification token" do
+    skip "Unit test that a mailer is being sent out"
+    @user = users(:two)
+
+    assert_enqueued_with(job: SendEmailAddressVerificationJob) do
+      @user.send_email_address_verification_token
+    end
+  end
+
   test "should respond to email_address_verified?" do
     @user = users(:one)
     assert_respond_to @user, :email_address_verified?
+  end
+
+  test "should set verified to true if token matches" do
+    @user = users(:two)
+    @user.email_address_verified?("TH0s7wfHfzqFWTMB21pRgy9aTrMlDecFv")
+    assert_equal true, @user.verified
   end
 end
