51e9764ec36391dfb8b3d3abc9f3b4f76e23e1ae
David Santoso <david.e.santoso@gmail.com>
2015-03-17 22:36:08 -0400

    Simple scope to query records but exclude any unwanted columns

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -9,6 +9,11 @@ class Authentication < ActiveRecord::Base
     self.challenge ||= SecureRandom.hex(32)
   end
 
+  # Simple scope to return columns except the ones passed in
+  def self.without_columns *columns
+    select(column_names - columns.map(&:to_s))
+  end
+
   def validate_user_is_verified
     errors.add(:user_id, "email address has not been verified") if !self.user.verified
   end
