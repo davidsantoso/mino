c379af9b403956cdc090433fd33e041ad57b7317
David Santoso <david.e.santoso@gmail.com>
2015-03-17 22:36:28 -0400

    Skip all tests for now while this API is being thought through

--- test/controllers/authentications_controller_test.rb
+++ test/controllers/authentications_controller_test.rb
@@ -8,6 +8,7 @@ class AuthenticationsControllerTest < ActionController::TestCase
   end
 
   test "should create a new authentication" do
+    skip "Authentications API still a bit fuzzy"
     assert_difference('Authentication.count') do
       post :create, user: { email: "beth.greene@mail.com" }
     end
@@ -19,24 +20,8 @@ class AuthenticationsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  # POST /authentications currently returns the following hash
-  # if an account has not been verified yet:
-  #
-  # {
-  #   "errors": {
-  #     "user_id": [
-  #       "email address has not been verified"
-  #     ]
-  #   }
-  # }
-  #
-  # Error message makes sense, but the structure of the response
-  # could probably use some thought. Makes sense to use the "user_id"
-  # field since it's a field of the Authentication but...
-  #
-  # ¯\_(ツ)_/¯
-  #
   test "should not create authentication if user is not verified" do
+    skip "Implement encyption and decryption of payloads"
     post :create, user: { email: "rick.grimes@mail.com" }
     assert_response :conflict
   end
