5b37b6716332cbbb7f46247b8efeff67107b5ebb
David Santoso <david.e.santoso@gmail.com>
2015-03-17 22:18:18 -0400

    Include lib in autoload

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Mino
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Autoload lib directory
+    config.autoload_paths << Rails.root.join('lib')
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
