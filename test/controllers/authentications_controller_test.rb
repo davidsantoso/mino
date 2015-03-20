a767651430a21da3523517b338c882d168d21720
David Santoso <david.e.santoso@gmail.com>
2015-03-19 22:31:14 -0400

    Remove older tests that aren't necessary at this point

--- test/controllers/authentications_controller_test.rb
+++ test/controllers/authentications_controller_test.rb
@@ -8,8 +8,8 @@ class AuthenticationsControllerTest < ActionController::TestCase
   end
 
   test "should create a new authentication" do
-    skip "Authentications API still a bit fuzzy"
     assert_difference('Authentication.count') do
+      # This needs to be updated with the correct encrypted request body
       post :create, user: { email: "beth.greene@mail.com" }
     end
 
@@ -21,24 +21,8 @@ class AuthenticationsControllerTest < ActionController::TestCase
   end
 
   test "should not create authentication if user is not verified" do
-    skip "Implement encyption and decryption of payloads"
+    # This needs to be updated with the correct encrypted request body
     post :create, user: { email: "rick.grimes@mail.com" }
     assert_response :conflict
-  end
-
-  test "should return 403 if challenge is not decrypted" do
-    skip "Implement challenge decryption check"
-    patch :update, authentication: { challenge: "the_incorrect_challenge" }
-    assert_response :forbidden
-  end
-
-  test "should activate authentication" do
-    skip "Implement authenticaton activation"
-    patch :update, authentication: { challenge: "52c23fb73c39eaabc6592a6279e61e1a901df93c13d418dadcd3e135e5555675" }
-
-    authentication = assigns(:authentication)
-
-    assert session[:token], authentication.token
-    assert_response :ok
   end
 end
