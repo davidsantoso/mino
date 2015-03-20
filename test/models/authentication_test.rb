1df3bd31e0417f55e24a3dde7fc6a9c503730602
David Santoso <david.e.santoso@gmail.com>
2015-03-19 22:31:55 -0400

    Remove tests that aren't necessary anymore/atm

--- test/models/authentication_test.rb
+++ test/models/authentication_test.rb
@@ -1,19 +1,6 @@
 require 'test_helper'
 
 class AuthenticationTest < ActiveSupport::TestCase
-  # TODO: This should work? Controller test verifies it gets created
-  # should validate_presence_of :challenge
-  test "should validate presence of challenge" do
-    skip "Write test directly above"
-  end
-
-  # TODO: Find best way to test uniquness on these two attributes
-  # should validate_uniqueness_of :challenge
-  # should validate_uniqueness_of :token
-  test "should validate uniquness" do
-    skip "Write test directly above"
-  end
-
   test "should respond to activate" do
     @authentication = authentications(:one)
     assert_respond_to @authentication, :activate
