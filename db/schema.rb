84e36acc40a6a22e47fab64a861d488ce9fe6c43
David Santoso <david.e.santoso@gmail.com>
2015-03-28 10:55:37 -0400

    Clean slate with authentications

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304023631) do
+ActiveRecord::Schema.define(version: 20150328145311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.integer  "user_id",                                null: false
-    t.string   "challenge",  limit: 255
-    t.string   "token",      limit: 255
-    t.boolean  "active",                 default: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                    limit: 255,                 null: false
