c8f4fb0cd4b9a3301619a5cad3cf853d472122ef
David Santoso <david.e.santoso@gmail.com>
2015-04-13 21:25:04 -0400

    Fixes tests for client verification

--- test/controllers/verifications_controller_test.rb
+++ test/controllers/verifications_controller_test.rb
@@ -25,7 +25,7 @@ class VerificationsControllerTest < ActionController::TestCase
     assert_equal true, @user.verified
   end
 
-  test "verification should render verification view if token does match" do
+  test "verification should render verification view if token and email match" do
     # setup sets headers to application/json by default since it's normally
     # just responding to API requests so we need to override for this test
     @request.headers["Accept"] = "text/html"
@@ -38,26 +38,27 @@ class VerificationsControllerTest < ActionController::TestCase
   end
 
   test "verification should return 403 if token exists but client signature does not match" do
-    get :index, { "signature" => "the_wrong_signature", "token" => "" }
+    get :index, { "signature" => "the_wrong_signature", "token" => "gr4yamjzehNf_eKFt9YY0kucAH-XWaH-M0A9yR5mj6Z6cfrqmyvjfAj4Q5kbXoRAs7nUmeVoKXtpv65X9_CWKQ==" }
     assert_response :forbidden
   end
 
-  test "verification should set user to verified true if token and email match" do
-    get :index, { "signature" => "", "token" => "" }
+  test "verification should set client to verified true if token and signature match" do
+    get :index, { "signature" => "7a7ae245b876a9f00420946aee5b6caf6666ffbb5d1894e9dce5cfa772e2e00a7958049a6653aa207ae6ace9f646c96033b3cc20dc16e6b8cdd275310abdb19f", "token" => "gr4yamjzehNf_eKFt9YY0kucAH-XWaH-M0A9yR5mj6Z6cfrqmyvjfAj4Q5kbXoRAs7nUmeVoKXtpv65X9_CWKQ==" }
 
-    @user = users(:one)
+    @client = clients(:one)
 
     assert_response :ok
-    assert_equal true, @user.verified
+    assert_equal true, @client.verified
+    assert_equal true, @client.enabled
   end
 
-  test "verification should render verification view if token does match" do
+  test "verification should render verification view if token and signature match" do
     # setup sets headers to application/json by default since it's normally
     # just responding to API requests so we need to override for this test
     @request.headers["Accept"] = "text/html"
     @request.headers["Content-Type"] = "text/html"
 
-    get :index, { "signature" => "", "token" => "" }
+    get :index, { "signature" => "7a7ae245b876a9f00420946aee5b6caf6666ffbb5d1894e9dce5cfa772e2e00a7958049a6653aa207ae6ace9f646c96033b3cc20dc16e6b8cdd275310abdb19f", "token" => "gr4yamjzehNf_eKFt9YY0kucAH-XWaH-M0A9yR5mj6Z6cfrqmyvjfAj4Q5kbXoRAs7nUmeVoKXtpv65X9_CWKQ==" }
 
     assert_response :ok
     assert_template :index
