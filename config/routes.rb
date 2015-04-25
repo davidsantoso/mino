4f4e1cdc0322feee18017138490e44f9ab6a1b36
David Santoso <david.e.santoso@gmail.com>
2015-04-24 22:02:03 -0400

    Initial scaffold for secrets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :update, :destroy]
   resources :verifications, only: [:index]
+  resources :secrets, only: [:create, :update, :destroy]
 end
