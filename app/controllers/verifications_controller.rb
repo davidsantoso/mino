78491e36c9e43e8145da0106dc28083407b9e995
David Santoso <david.e.santoso@gmail.com>
2015-04-27 22:23:45 -0400

    Use updated find_by syntax

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -8,7 +8,7 @@ class VerificationsController < ApplicationController
   # a client, but since at the moment it's going to be in the form of a
   # link in an email, GET is really the only option.
   def index
-    verification = Verification.includes(:verifiable).find_by_token(params[:token])
+    verification = Verification.includes(:verifiable).find_by(token: params[:token])
 
     if verification && !verification.expired && verification.verifiable.verify(params)
       verification.expired = true
