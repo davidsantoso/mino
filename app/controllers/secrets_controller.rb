b92bd95ff26fd00b916574eb356b80b06f35fd54
David Santoso <david.e.santoso@gmail.com>
2015-05-02 11:35:28 -0400

    Create secrets from authenticated decrypted messages

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,2 +1,35 @@
 class SecretsController < ApplicationController
+  # POST /secrets
+  def create
+    user = User.find_by(public_key: params[:public_key])
+    @secret = user.secrets.create(secret_create_params)
+
+    if @secret.errors.any?
+      render json: @encrypted_response.build(@secret.errors), status: :conflict
+    else
+      render json: @encrypted_response.build(@secret), status: :created
+    end
+  end
+
+  # PUT /secrets/:id
+  def update
+
+  end
+
+  # DELETE /secrets/:id
+  def destroy
+
+  end
+
+  private
+
+  # @data instance variable is being used instead of params because
+  # there is the decrypt preprocess happening in application controller
+  def secret_create_params
+    @request_params.require(:secret).permit(:name,
+                                            :url,
+                                            :username,
+                                            :password,
+                                            :notes)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :public_key, :encrypted_private_key, :nonce, :salt
   validates_uniqueness_of :email, :public_key, :encrypted_private_key
 
+  has_many :secrets, dependent: :destroy
   has_many :clients, dependent: :destroy
   has_many :verifications, as: :verifiable, dependent: :destroy
 
